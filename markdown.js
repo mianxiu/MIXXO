2ce6a5dc7df46335180f295b0f5edbf07f62b9a7
mianxiu <mianxiu8481@gmail.com>
2018-03-09 23:23:09 +0800

    beta

--- markdown.js
+++ markdown.js
@@ -41,7 +41,7 @@ function frontMatter(fromTags, imageTag) {
  */
 function cover(fromText, imageTag) {
   let result = fromText
-  let heading, h1, h2, h3, h4, h5, h6, img, link, youtube, p
+  let heading, h1, h2, h3, h4, h5, h6, img, link,code , youtube, p
   heading = {
     h1: '^# .+',
     h2: '^## .+',
@@ -52,9 +52,11 @@ function cover(fromText, imageTag) {
   }
 
   img = /\!\[\]\(.+?\)|\!\[.+?\]\(.+?\)/
-  link = /\[(?!<a).+?\]\((?!<a).+?\)(?!\])/
+  link = /\[(?!<a).+?\]\((?!<a).+[^]]?\)(?!\])/
   youtube = /\[youtube:\(.+?\)\]/g
-  p = /^(?!<).*/
+  code = /`+.+?`+/g
+  codePre = /^```$/m
+  p = /^(?!<[^a])[^\r\n].*/gm
 
   let loop = function (text) {
 
@@ -74,7 +76,7 @@ function cover(fromText, imageTag) {
     let _toImg = function (e) {
       if (img.test(e)) {
         let i = e.match(img)[0].split('](')
-        result = e.replace(img, '<img src="' + i[1].slice(0, -1) + '" alt="' + i[0].slice(2) + '">')
+        result = e.replace(img, '<img src="' + imageTag + '_' + i[1].slice(0, -1) + '" alt="' + i[0].slice(2) + '">')
         _toImg(result)
       }
     }
@@ -90,31 +92,79 @@ function cover(fromText, imageTag) {
     }
     _toLink(result)
 
+    let _toCodePre = function (e) {
+      let p = e.match(/^```$/gm)
+      for(let i=1;i<p.length+1;i++){
+        if(i%2 !== 0){
+          result = result.replace(codePre,'<pre>')
+        }else{
+          result = result.replace(codePre,'</pre>')      
+        }
+      }     
+    }
+    _toCodePre(result)
+
+    let _toCode = function (e) {
+      if (code.test(e)) {
+        e.match(code).forEach(c=>{         
+          result = result.replace(c, '<code>' + c.slice(1,-1) + '</code>')    
+        })          
+      }
+    }
+    _toCode(result)
+
+    
+
     let _toYoutube = function (e) {
       if (youtube.test(e)) {
         result = e.replace(youtube,
           '<iframe class="_youtube" width="680" height="384" src="https://www.youtube-nocookie.com/embed/'
-          + e.match(youtube)[0].slice(10,-2).split('/').pop()
+          + e.match(youtube)[0].slice(10, -2).split('/').pop()
           + '?rel=0&amp;showinfo=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>')
         _toYoutube(result)
       }
     }
     _toYoutube(result)
 
+ // 
+    let _toP = function (e) {
+      let r = ''
+      e.split('\r\n').forEach(el=>{
+        if(el!==''){
+          r += el + '<%-br-%>'
+        }
+      })
 
-    
-  let _toP = function (e) {
-  
-    e.split('\r\n').forEach(el => {
-    if((p.test(el) || /^<a.*/m.test(el)) && el !==''){
-        result = result.replace(el,'<p>' + el + '</p>')
+      let pre = /<pre>.+?<\/pre>/mg
+      let preAry = []
+      r.match(pre).forEach(e=>{
+        preAry.push(e)
+      })
+      r = r.replace(new RegExp(pre),'<%-pre-%>')
+      r = r.replace(/\<%-br-%\>/g,'\r\n')
+
+      if(p.test(r)){
+        r.match(p).forEach(e=>{
+          if(!/^<[^a].+>/m.test(e)){
+            r = r.replace(e,'<p>' + e + '</p>')
+          }  
+        })
       }
-    })
-  }
-  _toP(result)
-  }
 
-
+      preAry.forEach(e=>{
+        r = r.replace(/\<%-pre-%\>/,'<pre>'
+        +e.slice(5,-6).replace(/<(?!%)/g,'&lt;')
+        +'</pre>')
+        r = r.replace(/\<%-br-%\>/g,'\r\n')
+      })
+      
+      console.log(preAry)
+      console.log(r)
+      result = r
+    }
+    _toP(result)
+    
+}
 
 
   loop(fromText)
